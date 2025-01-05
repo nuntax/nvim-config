fcf177f7bf1cee0088b74842aebe2c352ff6c78e
nuntax <privatchristoph1@gmail.com>
2025-01-05 03:01:16 +0100

    added copilot and support for hyde colors

--- init.lua
+++ init.lua
@@ -6,12 +6,10 @@ vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.clipboard = 'unnamedplus'
-
+vim.g.ntc = vim.fn.expand('~/.cache/nvim/highlights/')
 vim.keymap.set('n', '<leader>o', 'a<CR><Esc>O', { noremap = true, silent = true })
-
 require("config.lazy")
-vim.cmd("colorscheme kanagawa-dragon")
 vim.wo.number = true
-
+vim.cmd('highlight Normal guibg=NONE guifg=NONE ctermbg=NONE ctermfg=NONE')
 vim.wo.relativenumber = true
 
---
+++ lua/plugins/chameleon.lua
@@ -0,0 +1,10 @@
+return {
+ 
+  {
+    'NeViRAIDE/chameleon.nvim',
+    dependencies = { { 'NeViRAIDE/stigmata.nvim', dev = true } },
+    event = 'VeryLazy',
+    opts = { hyde = true },
+    dev = true
+  },
+}
